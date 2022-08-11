78f29691da26db625a993bfe2f1a373dbf8dc1a8
Adam Hulewicz <hulewicz@Metrix-Metal.local>
2022-08-11 12:37:28 +0200

    dodano panel CRUD dla "pracownicy"

--- plum/admin.py
+++ plum/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from . import models 
+class PracownicyItemAdmin(admin.ModelAdmin): 
+    list_display = ('id', 'name', 'cardId', 'description', 'hasAdmin')
+    
+admin.site.register(models.Pracownicy, PracownicyItemAdmin)
 
 # Register your models here.
+"""
+from django.contrib import admin 
+    admin.site.register(models.FeedItem, FeedItemAdmin)
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=64)
+    cardId = models.IntegerField()
+    description = models.CharField(max_length=255)
+    hasAdmin = models.BooleanField()
+"""
